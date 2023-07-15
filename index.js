13efc48e4527207bf78cfc9fcd992c3201945ebe
dineshkumar-stack <stardinesh4@gmail.com>
2023-07-15 17:54:00 +0530

    Json content added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const http = require('http');
 
+const note = [
+
+    {
+        id: 1,
+        content: "Stack Clone",
+        important: true,
+    },
+    {
+        id: 2,
+        content: "YouTube Clone",
+        important: false,
+    }
+
+];
+
+
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'content-Type': 'text/plain' });
-    response.end("Hello");
+    response.writeHead(200, { 'content-Type': 'application/json' });
+    response.end(JSON.stringify(note));
 
 });
 
 const PORT = 3005;
 app.listen(PORT);
 
-console.log(`****************server on running on port ${PORT}******************`);
+console.log(`****************Server Running on Port ${PORT}******************`);
